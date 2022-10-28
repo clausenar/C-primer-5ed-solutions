318a1939b1fdeb4540b996bd0e6b03064c83ed9d
Anders R. Clausen <anders.clausen@gu.se>
2022-10-28 11:23:39 +0200

    Create e02_07.cpp

---
+++ chapter_02/e02_07.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+int main(){
+    std::cout << "Who goes with F\145rgus?\012";
+    std::cout << "3.14e1L"<< std::endl; // type is long double
+    std::cout << "1024f"<< std::endl;// type is float
+    std::cout << "3.14L"<< std::endl;// type is long double
+    return 0;}
+

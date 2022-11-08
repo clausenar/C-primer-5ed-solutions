3d795f72e924cbd76b389a8186d41b610317b8f5
Anders R. Clausen <anders.clausen@gu.se>
2022-11-08 14:22:33 +0100

    Create e03_41.cpp

---
+++ chapter_03/e03_41.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main()
+{
+
+int int_arr[] = {0,1,2,3,4,5};
+
+vector<int> ivec(std::begin(int_arr), std::end(int_arr));
+
+
+}

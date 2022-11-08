003f8dfbdce2f4f376dd2d10ba40dae290cd145d
Anders R. Clausen <anders.clausen@gu.se>
2022-11-08 12:46:40 +0100

    Create e03_25.cpp

---
+++ chapter_03/e03_25.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main()
+{
+
+vector<int> scores(11);
+
+unsigned grade;
+
+for (unsigned grade; cin ; /* */)
+if (grade <=100)
+++*(scores.begin() + grade/10);
+
+
+}

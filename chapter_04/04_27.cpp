34289e85d5f124ae5ef9409aaa53c598053de929
Anders R. Clausen <anders.clausen@gu.se>
2022-11-14 14:46:44 +0100

    Create 04_27.cpp

---
+++ chapter_04/04_27.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+
+unsigned long ul1=3, ul2=7;
+
+cout << "a: ul1 & ul2: " <<(ul1 & ul2) << "\n";
+cout << "b: ul1 | ul2: " <<(ul1 | ul2) << "\n";
+cout << "c: ul1 && ul2: " <<( ul1 && ul2) << "\n";
+cout << "d: ul1 && ul2: " << (ul1 || ul2) << "\n";
+
+
+}
+
+

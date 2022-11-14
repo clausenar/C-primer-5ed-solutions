917a9e1cc8670e5519f501b6024f5fea253f0576
Anders R. Clausen <anders.clausen@gu.se>
2022-11-14 15:55:22 +0100

    Create e05_03.cpp

---
+++ chapter_05/e05_03.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+
+int sum=0;
+
+for (int i=0;i<11;++i)
+
+
+{cout << i << "\n";
+
+sum=sum+i;}
+
+cout << sum;
+
+}
+
+

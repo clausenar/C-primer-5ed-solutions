defedaeb6828359f0c6524f23dadfae1f9628847
Anders R. Clausen <anders.clausen@gu.se>
2022-11-14 14:34:22 +0100

    Create e04_11.cpp

---
+++ chapter_04/e04_11.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+int a=4;
+int b=3;
+int c=2;
+int d=1;
+
+if (a>b && b>c && c>d)
+cout << "correct";
+else
+cout << "this is not correct";
+}
+
+

351c8baca611e122057f1e466ea3da76039b87a0
Anders R. Clausen <anders.clausen@gu.se>
2022-11-15 09:35:44 +0100

    Create e05_16.cpp

---
+++ chapter_05/e05_16.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using std::cout; using std::cin; using std::endl; using std::string; using std::pair;
+
+int main()
+{ 
+   
+string str="hej med dig";
+
+for (int i=0;i<4;++i)
+cout << i ;
+
+int t=0;
+while (t<4)
+{cout <<t;
+++t;}
+
+}

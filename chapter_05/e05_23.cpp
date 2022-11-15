dad01dafb8861b4f0165dca1b494f5a0c12f3f8b
Anders R. Clausen <anders.clausen@gu.se>
2022-11-15 12:46:33 +0100

    Create e05_23.cpp

---
+++ chapter_05/e05_23.cpp
@@ -0,0 +1,26 @@
+    #include <iostream>
+    #include <string>
+    #include <vector>
+    using namespace std;
+
+
+   
+    
+    auto division(double int1, double int2){
+        double d=int1/int2;
+        return d;
+    }
+
+     int main()
+    { 
+
+    int int1,int2;
+    double result;
+
+    cin >> int1 >> int2;
+    
+    cout << division(int1,int2);
+
+
+
+    }

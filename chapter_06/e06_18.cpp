8d99e2ea26e2eae9d6a9c81c5209116bd1571d8a
Anders R. Clausen <anders.clausen@gu.se>
2022-11-18 14:03:56 +0100

    Create e06_18.cpp

---
+++ chapter_06/e06_18.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cstring>
+#include<locale>
+
+using namespace std;
+
+int main()
+{
+    bool compare(matrix &m1, matrix &m2);
+    vector<int>::iterator change_val(int,vector<int>::iterator);
+}
